90021069a57245df6801022c19b312cd4ee4dbf2
yazuka000 <yazuka.000@gmail.com>
2022-08-10 09:21:06 +0900

    complete redux's basic skills

--- redux-practice/src/components/Counter.js
+++ redux-practice/src/components/Counter.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import { useSelector, useDispatch, connect } from "react-redux";
 
-import { counterActions } from "../store/index";
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
