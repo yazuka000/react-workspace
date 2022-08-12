ebb4967499f050f1137e874216f08de33dfbe07a
yazuka000 <yazuka.000@gmail.com>
2022-08-12 09:13:34 +0900

    now leraning advanced redux

--- redux-cart/src/store/cart-slice.js
+++ redux-cart/src/store/cart-slice.js
@@ -7,6 +7,10 @@ const cartSlice = createSlice({
     totalQuantity: 0
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -31,8 +35,7 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === id);
 
       state.totalQuantity--;
-      state.changed = true;
-      
+
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
