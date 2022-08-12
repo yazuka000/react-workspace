255dbb831533c9fe68d2a4e66394fed8c0e61254
yazuka000 <yazuka.000@gmail.com>
2022-08-12 10:25:34 +0900

    useEffect is using now in advanced redux

--- redux-cart/src/App.js
+++ redux-cart/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -5,7 +6,15 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://yazuka-react-http-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
 
   return (
     <Layout>
