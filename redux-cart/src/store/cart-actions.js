5caae0b8565c5e2d8d993019e552218df344a872
yazuka000 <yazuka.000@gmail.com>
2022-08-13 11:29:05 +0900

    complete advanced redux

--- redux-cart/src/store/cart-actions.js
+++ redux-cart/src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -46,7 +51,10 @@ export const sendCartData = (cart) => {
         "https://yazuka-react-http-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart)
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity
+          })
         }
       );
 
--- redux-cart/src/store/cart-slice.js
+++ redux-cart/src/store/cart-slice.js
@@ -4,7 +4,8 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
-    totalQuantity: 0
+    totalQuantity: 0,
+    changed: false
   },
   reducers: {
     replaceCart(state, action) {
@@ -16,6 +17,7 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === newItem.id);
 
       state.totalQuantity++;
+      state.changed = true;
 
       if (!existingItem) {
         state.items.push({
@@ -35,11 +37,13 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === id);
 
       state.totalQuantity--;
+      state.changed = true;
 
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     }
   }
