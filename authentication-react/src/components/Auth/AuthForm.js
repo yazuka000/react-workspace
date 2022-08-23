1319b65869390f738862d0b69b304c2d92844d28
yazuka000 <yazuka.000@gmail.com>
2022-08-23 09:34:10 +0900

    add login ui change

--- authentication-react/src/components/Auth/AuthForm.js
+++ authentication-react/src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useContext } from "react";
 
+import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -60,7 +63,8 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
