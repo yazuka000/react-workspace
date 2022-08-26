c5a3c1e9559c7a3b047fb13aa0c98fc4d2f0c5cd
yazuka000 <yazuka.000@gmail.com>
2022-08-26 09:29:51 +0900

    learn typescript's basic types

---
+++ typescript-practice/basics.ts
@@ -0,0 +1,78 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 24;
+
+age = 12;
+
+let userName: string | string[];
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let person: Person;
+
+// let person: {
+//   name: string;
+//   age: number;
+// };
+
+person = {
+  name: "Max",
+  age: 32
+};
+
+// person = {
+//   isEmployee: true
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type interface
+
+let course: string | number = "React - The Complete Guide";
+
+course = 12341;
+
+// Functions & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updateArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const StringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+// updateArray[0].split("");
