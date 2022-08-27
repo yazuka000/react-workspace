efe1689b7bdd7212409792368aff37622542d26c
yazuka000 <yazuka.000@gmail.com>
2022-08-27 09:58:19 +0900

    learn how to define model by TypeScript

--- typescript-practice/react-ts/src/components/Todos.tsx
+++ typescript-practice/react-ts/src/components/Todos.tsx
@@ -1,10 +1,14 @@
-function Todos() {
+import React from "react";
+import Todo from "../models/todo";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
