9f545baa8a5300bf7ea5725fed23940cb213f95b
yazuka000 <yazuka.000@gmail.com>
2022-08-28 14:01:51 +0900

    Modify contextAPI and comlpete Todo app in TypeScript

--- typescript-practice/react-ts/src/index.tsx
+++ typescript-practice/react-ts/src/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
